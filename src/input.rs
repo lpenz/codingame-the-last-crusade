63eaeb678053d6f2f83b12e09ff7c70d09793d73
Leandro Lisboa Penz <lpenz@lpenz.org>
2024-09-10 21:46:12 +0100

    We have to use edition=2018 with codingame

--- src/input.rs
+++ src/input.rs
@@ -2,6 +2,7 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
+use std::convert::{TryFrom, TryInto};
 use std::io;
 use std::str::FromStr;
 
--- tests/episode1.rs
+++ tests/episode1.rs
@@ -2,6 +2,7 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 
+use std::convert::TryFrom;
 use std::iter;
 
 use anyhow::Result;
